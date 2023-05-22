4cd83b2373be02d559ced90ffa9c4c21ab2d4389
mhkim0314-KAIST <88765333+mhkim0314-KAIST@users.noreply.github.com>
2023-05-22 17:07:40 +0900

    Add files via upload

---
+++ 2169_로봇청소기.py
@@ -0,0 +1,25 @@
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+
+# 맨 윗줄
+for j in range(1, M):
+    board[0][j] += board[0][j - 1]
+
+# 나머지
+for i in range(1, N):
+    startLeft = [board[i][j] + board[i - 1][j] for j in range(M)]
+    startRight = [board[i][j] + board[i - 1][j] for j in range(M)]
+
+    # →
+    for j in range(1, M):
+        startLeft[j] = max(startLeft[j], startLeft[j - 1] + board[i][j]) # 위에서 오는거 vs 왼쪽
+
+    # ←
+    for j in range(M - 2, -1, -1):
+        startRight[j] = max(startRight[j], startRight[j + 1] + +board[i][j]) # 위에서 오는거 vs 오른쪽에서 오는거
+
+    # 선택
+    for j in range(M):
+        board[i][j] = max(startLeft[j], startRight[j])
+
+print(board[-1][-1])
